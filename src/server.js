d7c9f5e4e6eea673f4b173ca3a592381dd899abf
Michael Hampson <mdavidhampson@gmail.com>
2024-09-30 15:29:38 +0100

    enhancement: add basic server and database

--- src/server.js
+++ src/server.js
@@ -4,5 +4,24 @@ const cors = require("cors");
 
 const User =require("./users/model");
 
-userRouter = require("./user/routes");
 
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+const syncTables = () => {
+    User.sync();
+};
+
+app.get("/health", (req, res) => {
+    res.status(200).json({message: "API is healthy"});
+});
+
+app.listen(port, () => {
+    syncTables();
+    console.log(`server is listening on port ${port}`);
+});
