4b81437fc77a0cd641e9beedc6720149f31fce38
Michael Hampson <mdavidhampson@gmail.com>
2024-10-01 15:31:46 +0100

    auth file completed and add salt

--- src/users/routes.js
+++ src/users/routes.js
@@ -2,10 +2,10 @@ const {Router} = require("express");
 const userRouter = Router();
 const {addUser} = require("./controllers");
 const {login} = require("./controllers");
+const { hashPass } = require("../middleware/auth");
+const {comparePass} = require("../middleware/auth")
 
-userRouter.post("/users/signup", addUser);
-userRouter.post("/users/login", login);
-
-
+userRouter.post("/users/signup", hashPass, addUser);
+userRouter.post("/users/login", comparePass, login);
 
 module.exports = userRouter;
